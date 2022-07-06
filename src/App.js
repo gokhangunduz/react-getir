985a4c095419274a1e38622d1e4dbaa2356e2171
gokhangunduz <mail@gokhangunduz.com.tr>
2022-07-07 00:02:24 +0300

    Fixed tailwind problems. Inıt base header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Header from './components/Header'
 function App() {
   return (
-    <div>test</div>
+    <>
+    <Header />
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}','./public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-color': '#4c3398',
+        'primary-color': '#5d3ebc',
+        'secondary-color': '#7849f7'
+      }
+    },
   },
   variants: {
     extend: {},
